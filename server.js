20957b7d1107629f48866b782cf1ae40498bdb1a
Andreas Røste <andreas.roste@rugge.org>
2018-08-07 13:52:19 +0200

    added /api path

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var http = require('http')
 var app = require('express')()
 
-app.get('/', function (req,res) {
+app.get('/api', function (req,res) {
+	res.status(200).json({
+		api: 'yup'
+	})
+})
+
+app.get('/:path', function (req,res) {
 	res.status(200).sendFile(__dirname + '/index.html')
 })
 
